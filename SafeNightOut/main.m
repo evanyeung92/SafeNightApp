74134074c01c3ff7f7c71f2e5a9b65cc36068a3c
evanyeung92 <evanyeung92@gmail.com>
2016-04-04 14:43:40 +1000

    Initial Commit

---
+++ SafeNightOut/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SafeNightOut
+//
+//  Created by Evan Yeung on 4/04/2016.
+//  Copyright © 2016 Evan Yeung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
